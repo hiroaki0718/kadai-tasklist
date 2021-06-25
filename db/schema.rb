6705517732c9b42ab62fa54c18662223ae56c9b3
hiroaki.nakamura <hironaka0718@gmail.com>
2021-06-25 12:09:43 +0000

    top page

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_130435) do
+ActiveRecord::Schema.define(version: 2021_06_23_113913) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "{:　type=>:User, :foreign_key=>true}_id"
+    t.index ["{:　type=>:User, :foreign_key=>true}_id"], name: "index_tasks_on_{:　type=>:User, :foreign_key=>true}_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
